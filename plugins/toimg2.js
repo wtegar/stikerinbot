600035db2fafaf5f23d6261274e39bd3fe33d94b
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 18:33:43 +0700

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', '© stikerin', m, false, { thumbnail: out })
+  await conn.sendFile(m.chat, out, 'out.png', '© Ollie', m, false, { thumbnail: out })
 }
 handler.help = ['toimg2']
 handler.tags = ['sticker']
