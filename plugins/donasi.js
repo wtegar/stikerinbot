b53656b4b68eef03f01d8a3eb4d00e077bffbb1a
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 18:27:18 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,8 @@
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'donasi', `
-┌「 *Donasi • Emoney* 」
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+┌「 *Donasi • Pulsa/GOPAY/OVO* 」
+├ 082264247854
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '© Ollie', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
