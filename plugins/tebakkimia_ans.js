d3783f05212ce110023473e884d92e6f3930b2d1
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 18:53:58 +0700

    Update tebakkimia_ans.js

--- plugins/tebakkimia_ans.js
+++ plugins/tebakkimia_ans.js
@@ -13,7 +13,7 @@ handler.before = async function (m) {
         if (['.teka', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.unsur.toLowerCase().trim()) {
             db.data.users[m.sender].exp += this.tebakkimia[id][2]
-            await this.sendButton(m.chat, benar + ` +${this.tebakkimia[id][2]} XP`, '© stikerin', 'Tebak Kimia', '.tebakkimia', m)
+            await this.sendButton(m.chat, benar + ` +${this.tebakkimia[id][2]} XP`, '© Ollie', 'Tebak Kimia', '.tebakkimia', m)
             clearTimeout(this.tebakkimia[id][3])
             delete this.tebakkimia[id]
         } else if (similarity(m.text.toLowerCase(), json.unsur.toLowerCase().trim()) >= threshold) m.reply(dikit)
