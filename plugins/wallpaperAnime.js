8f4d14303cd6543be0b5c5ef02d64602dbf7d262
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 23:18:15 +0700

    Update wallpaperAnime.js

--- plugins/wallpaperAnime.js
+++ plugins/wallpaperAnime.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat, img, '', '© Ollie', m, 0, { thumbnail: await (await fetch(img)).buffer() })
 }
 handler.help = ['wallpaperanime']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^(wallpaper|wp)anime$/i
 
 handler.limit = 1
