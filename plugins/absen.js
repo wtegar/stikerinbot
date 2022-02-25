e507bbe0730fc3433f1c138a78691890f48e7aa4
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 18:00:37 +0700

    Update absen.js

--- plugins/absen.js
+++ plugins/absen.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
-    if (!(id in conn.absen)) return await conn.sendButton(m.chat, `Tidak ada absen berlangsung!`, '© stikerin', 'Mulai', `${usedPrefix}mulaiabsen`, m)
+    if (!(id in conn.absen)) return await conn.sendButton(m.chat, `Tidak ada absen berlangsung!`, '© Ollie', 'Mulai', `${usedPrefix}mulaiabsen`, m)
     let absen = conn.absen[id][1]
     const wasVote = absen.includes(m.sender)
     if (wasVote) throw '*Kamu sudah absen!*'
@@ -22,7 +22,7 @@ ${conn.absen[id][2]}
 ├ Total: ${absen.length}
 ${list} 
 └────`.trim()
-    await conn.send2Button(m.chat, caption, '© stikerin', 'Absen', `${usedPrefix}absen`, 'Cek', `${usedPrefix}cekabsen`, m)
+    await conn.send2Button(m.chat, caption, '© Ollie', 'Absen', `${usedPrefix}absen`, 'Cek', `${usedPrefix}cekabsen`, m)
 }
 handler.help = ['absen']
 handler.tags = ['absen']
