3f6a4518783811e4ba14ea6e30281aff8beeab8f
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 17:36:19 +0700

    Update tod.js

--- plugins/tod.js
+++ plugins/tod.js
@@ -2,21 +2,21 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, usedPrefix }) => {
   if (/^tod$/i.test(command)) {
-    await conn.send3Button(m.chat, 'Truth or Dare', '© stikerin', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, 'Acak', `${conn.pickRandom([`${usedPrefix}dare`, `${usedPrefix}truth`])}`, m)
+    await conn.send3Button(m.chat, 'Truth or Dare', '© Ollie', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, 'Acak', `${conn.pickRandom([`${usedPrefix}dare`, `${usedPrefix}truth`])}`, m)
   }
   if (/^truth$/i.test(command)) {
     let res = await fetch(API('amel', '/truth', {}, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
-    conn.send2Button(m.chat, json.result, '© stikerin', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, m)
+    conn.send2Button(m.chat, json.result, '© Ollie', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, m)
   }
   if (/^dare$/i.test(command)) {
     let res = await fetch(API('amel', '/dare', {}, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
-    conn.send2Button(m.chat, json.result, '© stikerin', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, m)
+    conn.send2Button(m.chat, json.result, '© Ollie', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, m)
   }
 }
 handler.help = ['tod']
