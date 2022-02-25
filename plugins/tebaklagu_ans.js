19b7735aab4c0c81ad1e65e55d5a8ead5c8e6fbe
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 18:44:48 +0700

    Update tebaklagu_ans.js

--- plugins/tebaklagu_ans.js
+++ plugins/tebaklagu_ans.js
@@ -13,7 +13,7 @@ handler.before = async function (m) {
         if (['.cek', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.judul.toLowerCase()) {
             db.data.users[m.sender].exp += this.tebaklagu[id][2]
-            await this.sendButton(m.chat, benar + ` +${this.tebaklagu[id][2]} XP`, '© stikerin', 'Tebak Lagu', '.tebaklagu', m)
+            await this.sendButton(m.chat, benar + ` +${this.tebaklagu[id][2]} XP`, '© Ollie', 'Tebak Lagu', '.tebaklagu', m)
             clearTimeout(this.tebaklagu[id][3])
             delete this.tebaklagu[id]
         } else if (similarity(m.text.toLowerCase(), json.judul.toLowerCase().trim()) >= threshold) m.reply(dikit)
