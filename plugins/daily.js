43f503fb1ec7657db27d782fb97ca9c7afc14827
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 18:59:05 +0700

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -2,7 +2,7 @@ const free = 500
 const prem = 5000
 
 let handler = async (m, { conn, usedPrefix, isPrems }) => {
-  if (db.data.users[m.sender].level < 1) return await conn.sendButton(m.chat, 'Kamu masih level 0', '© stikerin', 'Naikan Level', `${usedPrefix}levelup`, m)
+  if (db.data.users[m.sender].level < 1) return await conn.sendButton(m.chat, 'Kamu masih level 0', '© Ollie', 'Naikan Level', `${usedPrefix}levelup`, m)
   let time = db.data.users[m.sender].lastclaim + 86400000
   if (new Date - db.data.users[m.sender].lastclaim < 86400000) throw `Kamu sudah mengklaim klaim harian hari ini\nTunggu selama ${conn.msToTime(time - new Date())} lagi`
   db.data.users[m.sender].exp += isPrems ? prem * db.data.users[m.sender].level : free * db.data.users[m.sender].level
