863d5f4f55400a835a8f62bed3c503fb750d0e0d
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 17:38:00 +0700

    ganti nama bot

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, text, usedPrefix, isAdmin, isOwner }) => {
     }
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
-    if (id in conn.vote) return conn.sendButton(m.chat, 'Masih ada vote di chat ini!', '© stikerin', 'Hapus', `${usedPrefix}-vote`, m)
+    if (id in conn.vote) return conn.sendButton(m.chat, 'Masih ada vote di chat ini!', '© Ollie', 'Hapus', `${usedPrefix}-vote`, m)
     conn.send2Button(m.chat, `Vote dimulai!
 
 *${usedPrefix}upvote* - untuk setuju
 *${usedPrefix}devote* - untuk tidak setuju
 *${usedPrefix}cekvote* - untuk mengecek vote
-*${usedPrefix}hapusvote* - untuk menghapus vote`, '© stikerin', 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, m)
+*${usedPrefix}hapusvote* - untuk menghapus vote`, '© Ollie', 'Upvote', `${usedPrefix}upvote`, 'Devote', `${usedPrefix}devote`, m)
     conn.vote[id] = [
         text,
         [],
