2b8fbb341388ca2f57da4040b219e57098c0dd80
wtegar <96050348+wtegar@users.noreply.github.com>
2022-02-25 18:38:10 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
     let json = await res.json()
     if (!json.status) throw json
     await m.reply(wait)
-    await conn.sendFile(m.chat, json.videoSD, 'tiktok.mp4', '© stikerin', m)
+    await conn.sendFile(m.chat, json.videoSD, 'tiktok.mp4', '© Ollie', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
